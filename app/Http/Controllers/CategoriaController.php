8374db44e46c2d08bf6b4b2084cdd272255f030f
caiofcosreal <caiofernandescoswosk@hotmail.com>
2023-03-04 11:54:43 -0300

    :sparkles: feat: Categoria Controller

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CategoriaRequest;
 use Illuminate\Http\Request;
 
 use App\Models\Categoria;
@@ -9,31 +10,60 @@ use App\Models\Categoria;
 class CategoriaController extends Controller
 {
 
+    private $categoria;
 
+    public function __construct(Categoria $categoria)
+    {
+        $this->categoria = $categoria;
+    }
 
     public function index()
     {
+        $categorias = $this->categoria->all();
+        return view('categoria.index', compact('categorias'));
     }
 
 
     public function create()
     {
+        return view('categoria.crud');
     }
 
 
-    public function store(Request $request)
+    public function store(CategoriaRequest $request)
     {
+        $data = $request->all();
+        $this->categoria->create($data);
+
+        return redirect()->route('categoria.index')->with('success', 'Categoria criada com sucesso!');
+    }
+
+    public function show($id)
+    {
+        $categoria = $this->categoria->find($id);
+        return response()->json($categoria);
     }
 
     public function edit($id)
     {
+        $categoria = $this->categoria->find($id);
+        return view('categoria.crud', compact('categoria'));
     }
 
-    public function update(Request $request, $id)
+    public function update(CategoriaRequest $request, $id)
     {
+        $data = $request->all();
+        $categoria = $this->categoria->find($id);
+        $categoria->update($data);
+
+        return redirect()->route('categoria.index')->with('success', 'Categoria alterada com sucesso!');
     }
 
     public function destroy($id)
     {
+        $categoria = $this->categoria->find($id);
+        $categoria->delete();
+
+        return redirect()->route('categoria.index')->with('success', 'Categoria deletada com sucesso!');
     }
 }
