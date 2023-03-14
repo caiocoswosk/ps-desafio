c71e5ce526be9fcd58dc66f4fa56a1418e20904e
caiofcosreal <caiofernandescoswosk@hotmail.com>
2023-03-14 16:52:32 -0300

    :sparkles: feat: ProdutoFactory, CategoriaFactory, ProdutoSeeder and CategoriaSeeder

--- database/factories/ProdutoFactory.php
+++ database/factories/ProdutoFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Categoria;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Produto>
@@ -17,7 +18,12 @@ class ProdutoFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'nome' => $this->faker->name($gender = null),
+            'descricao' => $this->faker->text($maxNbChars = 2000),
+            'preco' => $this->faker->randomFloat($nbMaxDecimals = 2, $min = 500, $max = 4000),
+            'quantidade' => $this->faker->numberBetween($min = 1, $max = 500),
+            'imagem' => $this->faker->imageUrl(640, 480, 'animals', true),
+            'categoria_id' => $this->faker->numberBetween(1, Categoria::count())
         ];
     }
 }
--- database/seeders/ProdutoSeeder.php
+++ database/seeders/ProdutoSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Produto;
 
 class ProdutoSeeder extends Seeder
 {
@@ -14,6 +15,6 @@ class ProdutoSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Produto::factory(50)->create();
     }
 }
